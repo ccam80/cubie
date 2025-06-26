94ee317b6a6f934bf9c032829e31b9af6f860c04
Chris Cameron <chrisjamescameron1@gmail.com>
2025-06-26 15:31:59 +1200

    Added SystemTester test class for easier creation of individual system tests. Now you just subclass this, add a python function with all the python bells and whistles that gives a correct answer, add some test cases, and it tests that your system instantiates, compiles, and gets the correct answer.

--- tests/SystemModels/test_genericODE.py
+++ tests/SystemModels/test_genericODE.py
@@ -1,6 +1,7 @@
 import pytest
 from tests.SystemModels.SystemTester import SystemTester
 from CuMC.SystemModels.genericODE import genericODE
+import numpy as np
 
 testsets=[
         # instantiate_settings is a tuple of (precision, state_names, parameter_names, observable_names, constants, num_drivers)
@@ -11,7 +12,23 @@ testsets=[
          "Single state, param, observable, no constants, 1 driver"),
         ((np.float64, ["x1", "x2"], ["p1", "p2"], ["o1"], {'c1': 2.0}, 2),
          (np.asarray([1.0, 0.0], dtype=np.float64), np.asarray([0.5, 5.5], dtype=np.float64), np.asarray([4.2, 1.8], dtype=np.float64)),
-         "Two states, two params, one observable, one constant, 2 drivers")
+         "Two states, two params, one observable, one constant, 2 drivers"),
+        ((np.float64, ["x1","x2","x3","x4","x5", "x6"], ["p1", "p2"], ["o1", "o2","o3","o4","o5","o6",], {'c1': 8.0}, 2),
+         (np.asarray([1.0, 0.0, 3.0, 4.0, 2.0, 5.0], dtype=np.float64), np.asarray([0.5, 5.5], dtype=np.float64), np.asarray([4.2, 1.8, 4.6, 1.7, 4.1, 2.3], dtype=np.float64)),
+         "More states and obs (slots) than params and constants (values)"),
+        ((np.float32, ["x0"], ["p"], ["o"], {}, 1),
+         (np.asarray([1.0], dtype=np.float32), np.asarray([2.0], dtype=np.float32),
+          np.asarray([3.0], dtype=np.float32)),
+         "Single state, param, observable, no constants, 1 driver"),
+        ((np.float64, ["x1", "x2"], ["p1", "p2"], ["o1"], {'c1': 2.0}, 2),
+         (np.asarray([1.0, 0.0], dtype=np.float64), np.asarray([0.5, 5.5], dtype=np.float64),
+          np.asarray([4.2, 1.8], dtype=np.float64)),
+         "Two states, two params, one observable, one constant, 2 drivers"),
+        ((np.float64, ["x1", "x2", "x3", "x4", "x5", "x6"], ["p1", "p2"], ["o1", "o2", "o3", "o4", "o5", "o6", ],
+          {'c1': 8.0}, 2),
+         (np.asarray([1.0, 0.0, 3.0, 4.0, 2.0, 5.0], dtype=np.float64), np.asarray([0.5, 5.5], dtype=np.float64),
+          np.asarray([4.2, 1.8, 4.6, 1.7, 4.1, 2.3], dtype=np.float64)),
+         "More states and obs (slots) than params and constants (values)")
     ]
 
 @pytest.mark.parametrize("instantiate_settings, input_data, test_name",
