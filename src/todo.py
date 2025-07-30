d3457bd23d17f030840ee690601151e289ccf152
Chris Cameron <chrisjamescameron1@gmail.com>
2025-07-30 12:12:20 +1200

    fix: remove doto #19

--- src/todo.py
+++ src/todo.py
@@ -1,4 +1,2 @@
 # Improvement: Currently forcing vectors are passed as an array, whereas a device function would be more flexible.
 #  The option of either would be more flexible again.
-# TODO [$6873084f7cdbf00008a72cfe]: CUDA-wide edits:
-#  - Add a stream argument propogated down through whatever layers require it.
