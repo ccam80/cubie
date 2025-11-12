5e4c1b62af26a8708ced84d24682af86f84b20c2
Chris Cameron <chrisjamescameron1@gmail.com>
2025-11-13 12:47:56 +1300

    fic: correct merge error

--- tests/odesystems/symbolic/test_cellml.py
+++ tests/odesystems/symbolic/test_cellml.py
@@ -247,8 +247,6 @@ def test_non_numeric_algebraic_equations_remain(beeler_reuter_model):
     algebraic_eq_count = len(observables) + len(auxiliaries)
     assert algebraic_eq_count > 0
 
-def test_import_the_big_boy(fabbri_linder_model):
-    assert fabbri_linder_model.num_states != 0
 def test_import_demir(demir_1999_model):
     assert demir_1999_model.num_states != 0
 
