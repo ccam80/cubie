0cdbe8c99e69b971485eba14bd57ab75ea4bd327
Chris Cameron <chrisjamescameron1@gmail.com>
2026-01-06 20:55:24 +1300

    chore: remove ai scaffolds

--- src/cubie/integrators/loops/ode_loop.py
+++ src/cubie/integrators/loops/ode_loop.py
@@ -388,23 +388,6 @@ class IVPLoop(CUDAFactory):
         fixed_mode = not config.is_adaptive
 
         @cuda.jit(
-            # [
-            #     (
-            #         precision[::1],
-            #         precision[::1],
-            #         precision[:, :, ::1],
-            #         precision[::1],
-            #         precision[::1],
-            #         precision[:, :],
-            #         precision[:, :],
-            #         precision[:, :],
-            #         precision[:, :],
-            #         precision[:,::1],
-            #         float64,
-            #         float64,
-            #         float64,
-            #     )
-            # ],
             device=True,
             inline=True,
             **compile_kwargs,
